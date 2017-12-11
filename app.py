5a8a641b23c6c8dcae0b8badce7e198be8657636
markonick <nicolas@kheironmed.com>
2017-12-11 10:52:25 +0000

    added results, pandas

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from flask import Flask
 from flask import render_template
 from redis import Redis
-#import pandas as pd
+import pandas as pd
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='app/templates')
 redis = Redis(host='redis', port=6379)
 
 
 def get_csv():
-    csv_path = './static/labels.csv'
-    csv_file = open(csv_path, 'r')
-    csv_obj = csv.DictReader(csv_file)
+    path = './app/static/labels.csv'
+    """csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)"""
+    df = pd.read_csv(path)
+    csv_obj = list(df)
     csv_list = list(csv_obj)
+
     return csv_list
+
 
 @app.route('/')
 def index():
     count = redis.incr('hits')
-    return 'Hello Pappefi and Markonick! You guys have been seen {} times.\n'.format(count)
+    return 'Hello Pappefi and Markonick! You guys have now been seen {} times.\n'.format(count)
+
 
 @app.route('/results')
 def csv_results():
-    template = 'csvresults.html'
+    template = 'results.html'
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
